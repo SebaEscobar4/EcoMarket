a7eed0181e483d062cbd038a0d875aac4807ed59
Nicolas <ncmonsalvez@gmail.com>
2025-06-14 21:35:01 -0400

    creacion TiendaModel

---
+++ EcoMarket/src/main/java/com/example/EcoMarket/model/TiendaModel.java
@@ -0,0 +1,32 @@
+package com.example.EcoMarket.model; 
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "Tienda " )
+
+
+
+public class TiendaModel {
+    @Id
+    private String id;
+
+    private String nombre;
+    private String Direccion;
+    private String telefono;
+    private String correo;
+    
+
+
+}
