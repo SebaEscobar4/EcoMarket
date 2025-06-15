3c1535b1d162271507e537ac5b9eda6eaf5cdcf8
Na1kox_AFK <matias.diaz.reyes2005@gmail.com>
2025-06-14 20:43:27 -0400

    Creacion de LoginService

--- EcoMarket/src/main/java/com/example/EcoMarket/repository/LoginRepository.java
+++ EcoMarket/src/main/java/com/example/EcoMarket/repository/LoginRepository.java
@@ -1,5 +1,17 @@
 package com.example.EcoMarket.repository;
 
-public class LoginRepository {
+
+import com.example.EcoMarket.model.LoginModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/*importacion de Java*/
+import java.util.Optional; 
+
+@Repository
+/*Hacemos una extencion para que obtenga todos los metodos del CRUD */
+public interface LoginRepository extends JpaRepository<LoginModel, String> {
+
+    Optional<LoginModel> findByRut(String rut);
 
 }
